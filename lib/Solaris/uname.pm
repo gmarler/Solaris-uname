d174d69d4220a81d12f9d4219466f90f1b6bc03f
Gordon Marler <gmarler@gmarler.com>
2015-07-30 15:34:51 -0400

    Add POD

--- lib/Solaris/uname.pm
+++ lib/Solaris/uname.pm
@@ -9,6 +9,19 @@ use XSLoader;
 
 # ABSTRACT: A Perl interface to uname(2) 
 
+=func uname()
+
+Returns a hashref with the following keys:
+
+=for :list
+* sysname    (equivalent of calling uname -s)
+* nodename   (equivalent of calling uname -n)
+* release    (equivalent of calling uname -r)
+* version    (equivalent of calling uname -v)
+* machine    (equivalent of calling uname -m)
+
+=cut
+
 XSLoader::load('Solaris::uname', $VERSION);
 
 1;
